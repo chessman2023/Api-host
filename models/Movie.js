473cc5f8770f28e451cb99c00522dcb343c1493b
chessman2023 <matinraahnama@gmail.com>
2022-12-05 17:02:34 +0100

    Api update

--- models/Movie.js
+++ models/Movie.js
@@ -2,55 +2,79 @@ const movies = [
     {
       "id" : "matfilm1",
       "title" : "13: Game of Death",
+      "year" : 2006,
+      "Genre" : "Comédie, Policier, Drame, Épouvante-Horreur, Thriller",
       "director" : "Chookiat Sakveerakul",
-      "shortDiscription" : "After losing his job, his car and his money, Phuchit, Krissada Sukosol, races against time to complete 13 tasks ordered by an anonymous caller who promised 100,000,000 Thai Baht upon completion.",
+      "shortDiscription" : "Peu de temps après avoir perdu son emploi et sa petite amie, Puchit reçoit un mystérieux coup de téléphone qui l'invite à participer à un jeu, avec à la clé 100 millions de baths (environ 2 millions d'euros) à gagner. Mais pour rafler la mise, Puchit doit d'abord accomplir 13 épreuves...",
       "totalTimeInminutes" : 114,
-      "imageUrl" : "13_game_of_death.jpg"
+      "watchedTimeInMinutes" : 114,
+      "imageUrl" : "13_game_of_death.jpg",
+      "linkyoutube" : "4lC5CdrhXkw"
       
     },
     {
       "id" : "matfilm2",
       "title" : "Alien",
+      "year" : 1979,
+      "Genre" : "Épouvante-Horreur, Science-fiction",
       "director" : "Ridley Scott",
-      "shortDiscription" : "The crew of a commercial spacecraft encounter a deadly lifeform after investigating an unknown transmission.",
+      "shortDiscription" : "En 2122, le vaisseau commercial Nostromo et son équipage en hibernation, font route vers la Terre avec une importante cargaison de minerai. Un signal, émanant d’une planète inconnue, les tire de leur sommeil artificiel. Intrigué, l’équipage se rend sur place et découvre les restes d'un gigantesque vaisseau extraterrestre. Lors de l’exploration de l’épave, l'officier Kane se fait agresser par une forme de vie inconnue.",
       "totalTimeInminutes" : 117,
-      "imageUrl" : "alien.jpg"
+      "watchedTimeInMinutes" : 117,
+      "imageUrl" : "alien.jpg",
+      "linkyoutube" : "cL5aAtL6Tok"
       
     },
     {
       "id" : "matfilm3",
       "title" : "Magnolia",
+      "year" : 1999,
+      "Genre" : "Drame",
       "director" : "Paul Thomas Anderson",
-      "shortDiscription" : "An epic mosaic of interrelated characters in search of love, forgiveness and meaning in the San Fernando Valley.",
+      "shortDiscription" : "Earl Partridge, âgé et malade, va bientôt mourir. Il demande à Phil, son infirmier, de retrouver Frank, le fils qu'il a jadis abandonné.",
       "totalTimeInminutes" : 188,
-      "imageUrl" : "magnolia.jpg"
+      "watchedTimeInMinutes" : 110,
+      "imageUrl" : "magnolia.jpg",
+      "linkyoutube" : "QYTqhmzROko"
       
     },
     {
       "id" : "matfilm4",
       "title" : "Donnie Darko",
+      "year" : 2001,
+      "Genre" : "Drame, Science-fiction",
       "director" : "Richard Kelly",
-      "shortDiscription" : "After narrowly escaping a bizarre accident, a troubled teenager is plagued by visions of a man in a large rabbit suit who manipulates him to commit a series of crimes.",
+      "shortDiscription" : "En octobre 1988, dans une coquette banlieue pavillonnaire de Virginie vit Donnie Darko, un adolescent pas comme les autres. Intelligent et débordant d'imagination mais aussi schizophrène. Il traite sa mère Elizabeth de tous les noms, laisse régulièrement des messages politiques pour son père Eddie sur le réfrigérateur et interpelle ses profs en plein cours. Somnambule, il parle la nuit avec un certain Frank, déguisé en lapin géant monstrueux. Un jour, ce dernier lui annonce que la fin du monde est imminente.",
       "totalTimeInminutes" : 113,
-      "imageUrl" : "donnie_darko.jpg"
+      "watchedTimeInMinutes" : 84,
+      "imageUrl" : "donnie_darko.jpg",
+      "linkyoutube" : "ZZyBaFYFySk"
       
     },
     {
       "id" : "matfilm5",
       "title" : "Dobermann",
+      "year" : 1997,
+      "Genre" : "Action, Policier, Thriller",
       "director" : "Jan Kounen",
-      "shortDiscription" : "Dobermann is the world's most ruthless bank robber and with his gang rob bank after bank, now in Paris. What can the police do but to let the mad, morally bankrupt police commissioner loose on him?",
+      "shortDiscription" : "Le Dobermann et son gang défraient la chronique. Banques, postes, fourgons, tout y passe. Une anthologie du braquage, un best-of du hold-up ! En face d'eux, un flic quelque peu pourri, qui fait de leur arrestation une affaire personnelle.",
       "totalTimeInminutes" : 94,
-      "imageUrl" : "dobermann.jpg"
+      "watchedTimeInMinutes" : 94,
+      "imageUrl" : "dobermann.jpg",
+      "linkyoutube" : "11qa0CBjlZ0"
       
     },
     {
       "id" : "matfilm6",
       "title" : "Starship Troopers",
+      "year" : 1997,
+      "Genre" : "Action, Science-fiction, Guerre",
       "director" : "Paul Verhoeven",
-      "shortDiscription" : "Humans in a fascist, militaristic future wage war with giant alien bugs.",
+      "shortDiscription" : "Au XXIVe siècle, la Terre est régentée par la toute-puissante Fédération, qui prône l'ordre et la vertu aux citoyens. Aux confins de la galaxie, une armée extraterrestre d'arachnides géants se dresse contre l'espèce humaine. Face à cette menace, cinq jeunes gens volontaires, s'engagent dans l'armée et partent en mission dans l'espace pour combattre les envahisseurs. Les insectes géants, terriblement organisés, s'avèrent bien plus intelligents et dangereux que prévu...",
       "totalTimeInminutes" : 129,
-      "imageUrl" : "starship_troopers.jpg"
+      "watchedTimeInMinutes" : 100,
+      "imageUrl" : "starship_troopers.jpg",
+      "linkyoutube" : "zPYuV_jGk7M"
       
     }
 ];
